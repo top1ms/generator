72bb741833146b37367ae09f645a88698fec7916
top1ms <top1zms@gmail.com>
2022-06-26 17:35:05 +0800

    1.0.7

--- src/main/java/com/code/generate/utils/PageUtils.java
+++ src/main/java/com/code/generate/utils/PageUtils.java
@@ -1,6 +1,6 @@
 package com.code.generate.utils;
 
-public class PageUtil {
+public class PageUtils {
     public static int getStartRow(int pageNum, int pageSize) {
         return pageNum > 0 ? (pageNum - 1) * pageSize : 0;
     }
